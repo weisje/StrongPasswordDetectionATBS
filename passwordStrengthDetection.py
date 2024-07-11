f9af8d9d92e2ae0d22e745c96bcbc50f1b611b9c
weisje <20269574+weisje@users.noreply.github.com>
2024-07-11 15:21:11 -0400

    imported re.  Added main() and passwordStrengthDetector() functions.  Added descriptive commenting for passwordStrengthDetector().

---
+++ passwordStrengthDetection.py
@@ -0,0 +1,19 @@
+import re
+
+
+def passwordStrengthDetector(stringToTest) -> bool:
+    """
+    Tests passed string to determine if it is at least 8 characters long, contains both an uppercase & lowercase character, and has at least one digit.
+    :param stringToTest: value that will be tested for criteria of a strong password
+    :type stringToTest: str
+    :return: bool
+    """
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
